d5bd2d1ce7b692158bba9cf3fafb5e0bead3abe2
CristinaIzquierdo2020 <7646cizquierdo@e-itaca.es>
2020-10-06 16:59:51 +0200

    .

--- src/ejerciciosManejoDeFicheros/Ejercicio2.java
+++ src/ejerciciosManejoDeFicheros/Ejercicio2.java
@@ -6,16 +6,18 @@ public class Ejercicio2 {
 	
 	public static void main(String[]args) 
 	{
-		try{
+		try
+		{
 	
 			FileWriter fw=new FileWriter("C:\\fichero1.txt");
 			FileReader fr=new FileReader("C:\\fichero1.txt");
 			
 			fw.write("Cien años de soleda");
-			fw.write(100);
+			fw.write(100);			
+			fw.flush();
 			
 			int valor = fr.read();
-			while(valor!=1) 
+			while(valor!=-1) 
 			{
 				System.out.println((char)valor);
 				valor = fr.read();
@@ -23,11 +25,11 @@ public class Ejercicio2 {
 			
 			fw.close();
 			fr.close();
-
-		}catch(IOException e) 
+		
+			
+		} catch(IOException e) 
 		{
 			System.out.println("Error");
 		}
-
 	}
 }
